49616cec5c70f92b2c8bada127e3f065a153c13e
Aditya yadav <adityayadavadi6100@gmail.com>
2024-12-14 19:58:54 +0530

    js codes

---
+++ script.js
@@ -0,0 +1,31 @@
+function display()
+{
+    console.log("hello world");
+}
+
+
+
+var name =  "aditya";
+var course = "full stack";
+var sername;
+
+
+function show()
+{
+ document.getElementById("demo").innerHTML = name+" "+course;
+}
+
+function cube()
+{
+    var sername;
+     var number = prompt("enter the number");
+
+     var result = number*number*number;
+    document.getElementById("demo").innerHTML = result;
+
+}
+
+function change()
+{
+    document.getElementById("demo").innerHTML 
+}
